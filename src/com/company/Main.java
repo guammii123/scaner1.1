c617ca2feb2c17cb3e5a84ed05c63ad02f9035a3
User2 <marcenkoadel651@gmail.com>
2022-01-07 01:16:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner good = new Scanner(System.in);
+        System.out.println("Ведите ваше имя:");
+        System.out.println("привет " + good.nextLine());
+
+    }
+}
+
